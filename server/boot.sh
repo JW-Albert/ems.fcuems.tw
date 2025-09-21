ac77bcd3f42dd9706e51182a49d9776936dbc45d
JW-Albert <ru04jo30801@gmail.com>
2025-09-21 21:42:14 +0800

    Refactor boot.sh to upgrade pip, setuptools, and wheel after virtual environment activation for improved dependency management

--- server/boot.sh
+++ server/boot.sh
@@ -9,9 +9,6 @@ sudo apt update -y && sudo apt upgrade -y
 echo "Installing dependencies python3 python3-pip python3-venv python3-dev..."
 sudo apt install -y python3 python3-pip python3-venv python3-dev
 
-echo "Upgrading pip setuptools wheel..."
-pip3 install --upgrade pip setuptools wheel
-
 echo "Changing directory..."
 cd /var/www/ems
 
@@ -20,6 +17,9 @@ python3 -m venv venv
 
 echo "Activating virtual environment..."
 source venv/bin/activate
+
+echo "Upgrading pip setuptools wheel..."
+pip3 install --upgrade pip setuptools wheel
 
 echo "Installing dependencies..."
 cd web
