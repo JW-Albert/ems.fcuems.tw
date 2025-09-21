8d46efcbeeddec508d46afc244df651a9586d405
JW-Albert <ru04jo30801@gmail.com>
2025-09-21 21:19:36 +0800

    Update Flask version and enhance boot and setup scripts for improved dependency management
    - Upgraded Flask from version 3.0.2 to 3.1.2 in requirements.txt.
    - Added detailed echo statements and error handling in boot.sh for better clarity during installation.
    - Improved setup.sh to include additional steps for copying server files and managing systemd service for EMS.

--- server/boot.sh
+++ server/boot.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
+
+echo "Installing dependencies set -e..."
+set -e
+
+echo "Updating system sudo apt update -y && sudo apt upgrade -y..."
 sudo apt update -y && sudo apt upgrade -y
 
+echo "Installing dependencies python3 python3-pip python3-venv python3-dev..."
 sudo apt install -y python3 python3-pip python3-venv python3-dev
 
+echo "Creating directory /var/www/ems..."
 mkdir -p /var/www/ems
 
+echo "Changing directory..."
 cd /var/www/ems
+
+echo "Creating virtual environment..."
 python3 -m venv venv
 
+echo "Activating virtual environment..."
 source venv/bin/activate
 
+echo "Installing dependencies..."
 cd web
 pip3 install -r requirements.txt
--- server/setup.sh
+++ server/setup.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+echo "Installing dependencies set -e..."
+set -e
+
+echo "Copying server..."
 cp server ../
 
+echo "Changing directory..."
 cd ..
+
+echo "Booting server..."
 ./server/boot.sh
 
+echo "Copying ems.service..."
 cp ems.service /etc/systemd/system/
 
+echo "Reloading systemctl..."
 systemctl daemon-reload
 systemctl enable ems
 systemctl start ems
