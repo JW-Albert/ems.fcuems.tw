6a620f9b489dd3a9dbff8eff9ff98f51f90aa8ba
JW-Albert <ru04jo30801@gmail.com>
2025-09-21 20:07:43 +0800

    Update boot script and service file for EMS setup
    - Create the directory structure for the EMS application.
    - Navigate to the EMS directory and set up a Python virtual environment.
    - Ensure the service file for the EMS Flask application has consistent formatting by removing an unnecessary newline.

--- server/boot.sh
+++ server/boot.sh
@@ -3,10 +3,12 @@ sudo apt update -y && sudo apt upgrade -y
 
 sudo apt install -y python3-pip python3-venv python3-dev
 
+mkdir -p /var/www/ems
+
+cd /var/www/ems
 python3 -m venv venv
 
 source venv/bin/activate
 
 cd web
 pip3 install -r requirements.txt
-
